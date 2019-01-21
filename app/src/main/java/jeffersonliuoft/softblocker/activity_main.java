5e009bcce6d296bfa364f413728be2f03622c4c1
jffersn <32963293+jffersn@users.noreply.github.com>
2019-01-20 22:41:43 -0500

    added buttons
    need to impliment notification function and help activity

--- app/src/main/java/jeffersonliuoft/softblocker/activity_main.java
+++ app/src/main/java/jeffersonliuoft/softblocker/activity_main.java
@@ -2,11 +2,17 @@ package jeffersonliuoft.softblocker;
 
 import android.app.Activity;
 import android.app.ActivityManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.Button;
 
 public class activity_main extends AppCompatActivity {
 
@@ -15,13 +21,37 @@ public class activity_main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        block();
+        Button btn1 = findViewById(R.id.onBtn);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                noti(true);
+            }
+        });
 
-        finish();
-        System.exit(0);
+        Button btn2 = findViewById(R.id.offBtn);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                noti(false);
+            }
+        });
+
+
+      //  block();
+
+     //   finish();
+       // System.exit(0);
 
     }
 
+
+    private void noti(boolean state){
+
+
+
+
+    }
     private void block(){ // blocks ad
 
         //pause the music
